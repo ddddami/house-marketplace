e9c8b49e198c4fbafe97b2f2359b30c14b41081e
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2022-09-17 05:46:08 +0100

    Create the marketplace app data model.

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longtitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class House(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    bathrooms = models.PositiveSmallIntegerField()
+    bedrooms = models.PositiveSmallIntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    promotions = models.ManyToManyField(Promotion, blank=True)
