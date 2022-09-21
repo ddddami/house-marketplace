511912b7295b03f6c3d044f1221ca46cb150b7b2
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2022-09-21 21:56:16 +0100

    Set coerce decimal to string to false.

--- house_marketplace/settings.py
+++ house_marketplace/settings.py
@@ -105,6 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
