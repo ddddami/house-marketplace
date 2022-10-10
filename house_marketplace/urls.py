c83033b010e2808b23c7894babc6d28e5a00e72b
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2022-10-10 20:49:46 +0100

    Add the homepage.

--- house_marketplace/urls.py
+++ house_marketplace/urls.py
@@ -22,6 +22,7 @@ admin.site.site_header = 'House marketplace Admin'
 admin.site.index_title = 'Admin'
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('auth/', include('djoser.urls')),
